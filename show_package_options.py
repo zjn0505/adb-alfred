5e5f441bab6146cfdbd8aa490a252a63bb5560fd
Charlie Zhang <charlie.zhang@robotemi.com>
2019-01-08 11:51:23 +0800

    Add extract apk option

--- show_package_options.py
+++ show_package_options.py
@@ -12,7 +12,7 @@ def main(wf):
     shell_cmd = "{0} -s {1} shell dumpsys package {2} | grep 'versionCode\|versionName' | awk '{{print $1}}'".format(adb_path, serial, packName)
 
     result = None
-
+    versionName = ""
     # Package info
     try:
         result = subprocess.check_output(shell_cmd, stderr=subprocess.STDOUT, shell=True)
@@ -21,7 +21,8 @@ def main(wf):
     if result:
         infos = result.rstrip().split('\n')
         log.debug(infos)
-        it = wf.add_item(title=packName, subtitle="{0}({1})".format(infos[1][12:], infos[0].strip()[12:]), valid=False, icon=ICON_INFO)
+        versionName = infos[1][12:]
+        it = wf.add_item(title=packName, subtitle="{0}({1})".format(versionName, infos[0].strip()[12:]), valid=False, icon=ICON_INFO)
 
     # App info
     title = "App info"
@@ -53,10 +54,18 @@ def main(wf):
                 arg="uninstall_app",
                 subtitle="`cmd` to keep data and cache",
                 valid=True)
-                
+
     mod = it.add_modifier("cmd", subtitle="keep the data and cache directories")
     mod.setvar("mod", "keep_data")
 
+    # Get apk file
+    title = "Extract apk file"
+    it = wf.add_item(title=title,
+                arg="extract_apk",
+                valid=True)
+    if versionName:
+        it.setvar("pretty_version", versionName)
+                
     wf.send_feedback()
 
 if __name__ == '__main__':
