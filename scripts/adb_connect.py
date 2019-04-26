d78d13342b6d229c7e474eafe2d250ff3c21a53e
Charlie Zhang <charlie.zhang@robotemi.com>
2019-04-26 14:43:49 +0800

    Update adb connect logic

--- scripts/adb_connect.py
+++ scripts/adb_connect.py
@@ -3,6 +3,9 @@ from toolchain import run_script
 
 adb_path = os.getenv('adb_path')
 ip = os.getenv('ip')
+if ip and ip[-5:] != ":5555":
+	ip = ip + ":5555"
+
 shell_cmd = '{0} connect {1}'.format(adb_path, ip)
 
 try:
