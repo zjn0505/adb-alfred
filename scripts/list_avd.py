43b28bbf29d8f7265545def9d2771bbf1eb6f172
Weslley Neri <weslley39@gmail.com>
2019-08-14 10:57:06 -0300

    fix for when func is none

--- scripts/list_avd.py
+++ scripts/list_avd.py
@@ -7,4 +7,7 @@ func = os.getenv("func")
 
 shell_cmd = "{0} -avd {1} {2}".format(emulator_path, sys.argv[1], func)
 
+if func is None:
+  shell_cmd = "{0} -avd {1}".format(emulator_path, sys.argv[1])
+
 result = run_script(shell_cmd)
