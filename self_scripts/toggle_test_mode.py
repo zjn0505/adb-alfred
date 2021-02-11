5689f48b35b7f234e5f9fd2f574aefc76622b623
Afzal Najam <AfzalivE@users.noreply.github.com>
2021-02-11 12:41:07 -0500

    Update toggle_test_mode.py

--- self_scripts/toggle_test_mode.py
+++ self_scripts/toggle_test_mode.py
@@ -10,18 +10,6 @@ get_window_animation_scale_cmd = "adb shell settings get global window_animation
 get_transition_animation_scale_cmd = "adb shell settings get global transition_animation_scale"
 get_animator_duration_scale_cmd = "adb shell settings get global animator_duration_scale"
 
-# $ANDROID_HOME/platform-tools/adb </dev/null -s $line wait-for-device shell '
-#             while [[ "$(getprop sys.boot_completed)" != "1" || "$(getprop init.svc.bootanim)" != "stopped" ]]; do sleep 1; done;
-#             svc power stayon true;
-#             settings put global window_animation_scale 0;
-#             settings put global transition_animation_scale 0;
-#             settings put global animator_duration_scale 0;
-#             settings put secure long_press_timeout 1500;
-#             settings put secure show_ime_with_hard_keyboard 0;
-#             settings put secure spell_checker_enabled 0;
-#             settings put secure autofill_service null;
-#             input keyevent 82;'
-
 get_window_animation_scale_cmd = "{0} -s {1} shell settings get global window_animation_scale".format(adb_path, serial)
 get_transition_animation_scale_cmd = "{0} -s {1} shell settings get global transition_animation_scale".format(adb_path, serial)
 get_animator_duration_scale_cmd = "{0} -s {1} shell settings get global animator_duration_scale".format(adb_path, serial)
