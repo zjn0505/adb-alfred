01991bfbf536b4866a1bfeafb0a580cf9c4c05f9
zjn0505 <zjn0505@hotmail.com>
2022-09-06 10:53:44 +0800

    Update dump package and extract apk commands

--- extract_apk.py
+++ extract_apk.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from toolchain import run_script
 from commands import CMD_SHOW_APK_PATH
 from commands import CMD_PULL_APK_TO_DESKTOP
@@ -9,7 +10,11 @@ prettyVersion = os.getenv('pretty_version')
 try:
 	result = run_script(CMD_SHOW_APK_PATH.format(package))
 	if result and result.startswith("package:"):
+		result = result.split('\n')[0]
 		path = result[8:].strip()
+		sys.stderr.write("\n Path: " + path + "\n")
+		sys.stderr.write("\n package: " + package + "\n")
+		sys.stderr.write("\n prettyVersion: " + prettyVersion + "\n")
 
 		run_script(CMD_PULL_APK_TO_DESKTOP.format(path, package, prettyVersion))
 		print("Apk extracted to desktop")
