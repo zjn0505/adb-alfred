a3a29d019865ebdc313461b399b250d62e00868e
zjn0505 <zjn0505@hotmail.com>
2022-11-12 00:32:28 +0800

    Fix pid may be unavailable in dump_stack results

--- dump_stack.py
+++ dump_stack.py
@@ -20,20 +20,24 @@ class Activity():
     def setPid(self, pid):
         self.pid = pid
         return self
+        
+    def __str__(self):
+        return f"Activity name {self.name}, package is {self.package}, affinity is {self.affinity}, pid is {self.pid}"
 
 def main(wf):
     # try:
     result = run_script(CMD_DUMP_STACK)
 
     stackData = result.split("\n")
-    log.debug("stackData")
-    log.debug(stackData)
+    # log.debug("stackData")
+    # log.debug(stackData)
 
     if stackData:
         activityList = []
+        log.debug("Start")
         for index in range(len(stackData)):
             data = stackData[index].strip()
-            if index % 3 == 0:
+            if data.find("* Hist #") >= 0:
                 # Hist: * Hist #0: ActivityRecord{118a7ef u0 com.roboteam.teamy.china/com.roboteam.teamy.home.HomeActivity t1564}
                 start = data.find(" u0 ") + 4
                 activityData = data[start:data.find(" ", start)]
@@ -45,26 +49,39 @@ def main(wf):
                     activityName = packageName + activityName
 
                 activityList.append(Activity(name=activityName, package=packageName))
-            elif index % 3 == 1:
+            elif data.find("app=ProcessRecord") >= 0:
                 # ProcessRecord:  app=ProcessRecord{8b2b5e1 1333:com.android.settings/1000}
-                activityList[-1] = activityList[-1].setPid(stackData[index].strip()[26:].split(":")[0])
-            elif index % 3 == 2:
+                log.debug("ProcessRecord")
+                log.debug(data)
+                activityList[-1] = activityList[-1].setPid(data[26:].split(":")[0])
+            elif data.find("taskAffinity=") >= 0:
                 # taskAffinity:    taskAffinity=com.android.settings
-                activityList[-1] = activityList[-1].setAffinity(stackData[index].strip()[13:])
+                log.debug("taskAffinity")
+                log.debug(data)
+                activityList[-1] = activityList[-1].setAffinity(data[13:])
         
         for item in activityList:
+            log.debug(item)
             if mod == "first_app" and item.package != activityList[0].package:
                 continue
             elif mod == "first_stack" and item.affinity != activityList[0].affinity:
                 continue
             simpleName = item.name.split('.')[-1]
+
+            subtitle = ""
+            if item.pid == None:
+                subtitle = 'Affinity: ' + item.affinity
+            else:
+                subtitle = 'Affinity: ' + item.affinity + " Pid: " + item.pid
+
             it = wf.add_item(title=item.name,
-                subtitle='Affinity: ' + item.affinity + " Pid: " + item.pid,
+                subtitle=subtitle,
                 autocomplete=simpleName,
-                match=simpleName,
+                match=item.name,
                 arg=item.name,
                 copytext=item.name,
                 valid=True)
+
             m = it.add_modifier("cmd", subtitle="Package: " + item.package)
             m.setvar("package", item.package)
             m = it.add_modifier("alt", subtitle="Uninstall: " + item.package, arg = item.package)
